5f4fb365ba0cbb9affe9a19626c238e23bcaa67f
shubhamkumar200328 <shubhamkumar200328@gmail.com>
2025-03-15 08:52:43 +0530

    Working on Phase-1 of serviceoffered section and 20 percent completed. commit-4

---
+++ src/app/loginsection/registerAsAdmin/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar"
+
+export default function RegisterasAdmin() {
+  return (
+    <>
+      <Navbar />
+      <div className="container text-4xl text-pink-400 m-14">
+        This page will appear as Admin registration page.
+      </div>
+    </>
+  )
+}
